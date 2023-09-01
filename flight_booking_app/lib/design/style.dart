044d98d064189c403198b20d6892759d4cfe073f
SupanutTha <supanut.thanajirawat@gmail.com>
2023-09-01 14:25:21 +0700

    -craete homepage -create toggle button

---
+++ flight_booking_app/lib/design/style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle customTextStyle = TextStyle(
+    //fontFamily: 'YourDesiredFontFamily',
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    // Add other style properties as needed
+  );
+
+  static const TextStyle titleTextStyle = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    // Add other style properties as needed
+  );
+
+  // Add more styles as needed
+}
