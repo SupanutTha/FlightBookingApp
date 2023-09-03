e5286286044cc5eb8833fa206b79ec7c809af687
SupanutTha <supanut.thanajirawat@gmail.com>
2023-09-04 00:22:11 +0700

    create loading screen

--- flight_booking_app/lib/screens/loading_screen.dart
+++ flight_booking_app/lib/screens/loading_screen.dart
@@ -1,11 +1,59 @@
+import 'package:flight_booking_app/screens/home_screen.dart';
+import 'package:flight_booking_app/utilities/create_db.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:flutter_splash_screen/flutter_splash_screen.dart';
 
-class LoadingScreen extends StatelessWidget {
+class LoadingScreen extends StatefulWidget {
+
+
   @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    createState();
+    hideScreen();
+  }
+
+  ///hide your splash screen
+  Future<void> hideScreen() async {
+    Future.delayed(Duration(milliseconds: 6000), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => HomePage(),
+      ));
+    });
+  }
+
+  Future<void> createState () async {
+     final createDb = CreateDb();
+    await createDb.createAirplaneAirlineDb();
+
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
+      //backgroundColor: const Color(0xFFEC441E),
       body: Center(
-        child: CircularProgressIndicator(), // You can use any loading indicator widget
+        child : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset('assets/json/lottie/animation_lm3no1er.json'),
+            Text(
+              'Go a Where',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                  color: const Color(0xFFEC441E),
+                  fontFamily: 'Amiri Quran Colored',
+                  fontSize: 40,
+                  fontWeight: FontWeight.normal,
+                  ),
+            ),
+          ],
+        )
       ),
     );
   }
---
+++ flight_booking_app/lib/screens/search_result_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatefulWidget{
+  @override
+  State<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold()
+  }
+
+
+}
